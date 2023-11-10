477d93373dbf7712d94ad279fbe616de5e0c99ae
hitsho <134806168+hitshoCodes@users.noreply.github.com>
2023-11-10 16:42:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
 import os
-import base64
 from urllib.parse import urljoin, urlparse
 from datetime import datetime
-import subprocess
 import time
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
-def download_image(img_tag, base_url):
-    img_url = img_tag['src']
+def download_image(img_url, save_dir):
+    try:
+        img_response = requests.get(img_url, stream=True)
 
-    if img_url.startswith("data:image"):
-        data = img_url.split(",")[1]
-        img_data = base64.b64decode(data)
-        img_filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_image.png"
-        with open(img_filename, 'wb') as img_file:
-            img_file.write(img_data)
-    else:
-        img_data = img_url.encode("utf-8")
-        img_filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{urlparse(img_url).path.split('/')[-1]}"
-        with open(img_filename, 'wb') as img_file:
-            img_file.write(img_data)
+        if img_response.status_code == 200:
+            total_size = int(img_response.headers.get('content-length', 0))
+            block_size = 1024
+            progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=f"Downloading {os.path.basename(urlparse(img_url).path)}")
 
-    img_tag['src'] = img_filename
+            img_filename = f"{datetime.now().strftime('%Y%m%d%H%M%S')}_{os.path.basename(urlparse(img_url).path)}"
+            img_path = os.path.join(save_dir, img_filename)
+
+            with open(img_path, 'wb') as img_file:
+                for data in img_response.iter_content(block_size):
+                    progress_bar.update(len(data))
+                    img_file.write(data)
+
+            progress_bar.close()
+            return img_path
+        else:
+            print(f"Failed to download image: {img_url}, Status code: {img_response.status_code}")
+            return None
+    except Exception as e:
+        print(f"Error downloading image: {e}")
+        return None
 
 def fix_relative_links(base_url, link):
     return urljoin(base_url, link)
@@ -33,64 +41,93 @@ def extract_favicon_url(soup, base_url):
         return urljoin(base_url, favicon_link.get("href"))
     return None
 
-def download_favicon(favicon_url, base_url):
-    if favicon_url:
-        favicon_response = requests.get(favicon_url)
+def download_favicon(favicon_url, save_dir):
+    try:
+        if favicon_url:
+            favicon_response = requests.get(favicon_url)
+            if favicon_response.status_code == 200:
+                favicon_data = favicon_response.content
+                favicon_filename = "favicon.ico"
+                favicon_path = os.path.join(save_dir, favicon_filename)
+                with open(favicon_path, 'wb') as favicon_file:
+                    favicon_file.write(favicon_data)
+    except Exception as e:
+        print(f"Error downloading favicon: {e}")
 
-        if favicon_response.status_code == 200:
-            favicon_data = favicon_response.content
-            favicon_filename = "favicon.ico"
-            with open(favicon_filename, 'wb') as favicon_file:
-                favicon_file.write(favicon_data)
+def delete_old_files(save_dir):
+    try:
+        for file in os.listdir(save_dir):
+            file_path = os.path.join(save_dir, file)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+    except Exception as e:
+        print(f"Error deleting old files: {e}")
 
-def delete_old_files():
-    current_directory = os.path.dirname(os.path.realpath(__file__))
-    files = os.listdir(current_directory)
-    for file in files:
-        if file != "main.py" and file != "LICENSE" and file != "README.md" and file != "requirements.txt":
-            os.remove(os.path.join(current_directory, file))
+def save_html(soup, save_dir):
+    try:
+        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+        output_file = os.path.join(save_dir, f'website_{timestamp}.html')
+        with open(output_file, 'w', encoding='utf-8') as file:
+            file.write(str(soup))
+        print(f"Website was saved to '{output_file}'.")
+    except Exception as e:
+        print(f"Error saving HTML: {e}")
 
-delete_old_files()
+def main():
+    save_dir = 'saved'
 
-url = input("Enter the URL: ")
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 
-if not url.startswith('http://') and not url.startswith('https://'):
-    url = 'https://' + url
+    delete_old_files(save_dir)
 
-response = requests.get(url)
+    url = input("Enter the URL: ")
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, 'html.parser')
-    base_url = response.url
-    favicon_url = extract_favicon_url(soup, base_url)
-    download_favicon(favicon_url, base_url)
-    img_tags = soup.find_all('img', src=True)
-    for img_tag in img_tags:
-        download_image(img_tag, base_url)
+    if not url.startswith('http://') and not url.startswith('https://'):
+        url = 'https://' + url
 
-    css_links = soup.find_all('link', rel='stylesheet')
-    for link in css_links:
-        css_url = urljoin(base_url, link.get('href'))
-        css_response = requests.get(css_url)
-        if css_response.status_code == 200:
-            css_content = css_response.text
-            style_tag = soup.new_tag("style")
-            style_tag.string = css_content
-            soup.head.append(style_tag)
-            link.extract()
+    try:
+        response = requests.get(url)
 
-    links = soup.find_all('a', href=True)
-    for link in links:
-        link['href'] = fix_relative_links(base_url, link['href'])
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            base_url = response.url
 
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    with open(f'website_{timestamp}.html', 'w', encoding='utf-8') as file:
-        file.write(str(soup))
+            favicon_url = extract_favicon_url(soup, base_url)
+            download_favicon(favicon_url, save_dir)
 
-    print(f"Website was saved to 'website_{timestamp}.html'.")
+            img_tags = soup.find_all('img', src=True)
+            for img_tag in img_tags:
+                img_url = urljoin(base_url, img_tag['src'])
+                img_path = download_image(img_url, save_dir)
+                if img_path:
+                    img_tag['src'] = os.path.basename(img_path)
+
+            css_links = soup.find_all('link', rel='stylesheet')
+            for link in css_links:
+                css_url = urljoin(base_url, link.get('href'))
+                css_response = requests.get(css_url)
+                if css_response.status_code == 200:
+                    css_content = css_response.text
+                    style_tag = soup.new_tag("style")
+                    style_tag.string = css_content
+                    soup.head.append(style_tag)
+                    link.extract()
+
+            links = soup.find_all('a', href=True)
+            for link in links:
+                link['href'] = fix_relative_links(base_url, link['href'])
+
+            save_html(soup, save_dir)
+
+        else:
+            print(f"Failed to copy the web page. Status code: {response.status_code}")
+
+    except requests.RequestException as e:
+        print(f"Error accessing the URL: {e}")
+
+    print("Download complete. Closing in 5 seconds...")
     time.sleep(5)
-    print("Closing in 5 seconds...")
-else:
-    print(f"Failed to copy the web page. Status code: {response.status_code}")
-    time.sleep(5)
-    print("Closing in 5 seconds...")
+
+if __name__ == "__main__":
+    main()
